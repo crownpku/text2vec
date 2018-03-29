3a83846997970c057ecc7a23a91d5a697e2b7466
crownpku <crownpku@gmail.com>
2018-03-29 11:17:08 +0200

    add more text2vec methods

--- README.md
+++ README.md
@@ -7,47 +7,63 @@ Goal of this repository is to build a tool to easily generate document/paragraph
 ## Usage
 
 We do such transformation by the following ways. 
+
 doc_list is a list of documents/paragraphs/sentences.
-
-1. Use TFIDF
-```
-import text2vec
-docs_tfidf = text2vec.get_tfidf(doc_list)
-```
-
-2. Use Average of Word Embeddings
-```
-import text2vec
-docs_avgw2v = text2vec.avg_wv(doc_list)
-```
-
-3. Use Weighted Word Embeddings wrt TFIDF
-```
-import text2vec
-docs_emb = text2vec.tfidf_weighted_wv(doc_list)
-```
-
-4. Use Gensim Doc2Vec (TODO)
-
-Either way we can train again with our own data, or directly use the pre-trained doc2vec/word2vec models.
-
-
-
-### Training (Optional) (TODO)
-
-* Initialize: Pre-trained Word Embeddings (Optional)
-* Input: Large Corpus
-* Output: Doc2Vec/Word2Vec models
-
-### Inference:
 
 * Initialize: Pre-trained Doc2Vec/Word2Vec model
 * input: List of Documents
 * output: List of Vectors of dimention N
 
 
+#### Use TFIDF
+```
+import text2vec
+docs_tfidf = text2vec.get_tfidf(doc_list)
+```
+
+#### Use Latent Semantic Indexing(LSI)
+```
+import text2vec
+docs_lsi = text2vec.get_lsi(doc_list)
+```
+
+#### Use Random Projections(RP)
+```
+import text2vec
+docs_rp = text2vec.get_rp(doc_list)
+```
+
+#### Use Latent Dirichlet Allocation(LDA)
+```
+import text2vec
+docs_lda = text2vec.get_lda(doc_list)
+```
+
+#### Use Hierarchical Dirichlet Process(HDP)
+```
+import text2vec
+docs_hdp = text2vec.get_hdp(doc_list)
+```
+
+#### Use Average of Word Embeddings
+```
+import text2vec
+docs_avgw2v = text2vec.avg_wv(doc_list)
+```
+
+#### Use Weighted Word Embeddings wrt. TFIDF
+```
+import text2vec
+docs_emb = text2vec.tfidf_weighted_wv(doc_list)
+```
+
+### Similarity Calculation
+Coming up soon
+
 
 ## Reference
+
+https://radimrehurek.com/gensim/tut2.html
 
 https://github.com/jhlau/doc2vec
 
--- text2vec.py
+++ text2vec.py
@@ -2,12 +2,13 @@ import spacy
 import pandas as pd
 from gensim.corpora import Dictionary
 from gensim.models.tfidfmodel import TfidfModel
+from gensim import corpora, models, similarities
 from gensim.matutils import sparse2full
 import numpy as np
 
 
 
-#### Functions to lemmatise docs
+# Functions to lemmatise docs
 def _keep_token(t):
     return (t.is_alpha and 
             not (t.is_space or t.is_punct or 
@@ -16,12 +17,26 @@ def _lemmatize_doc(doc):
     return [ t.lemma_ for t in doc if _keep_token(t)]
 
 
+
+    
+
+
 #Gensim to create a dictionary and filter out stop and infrequent words (lemmas).
 def _get_docs_dict(docs):
     docs_dict = Dictionary(docs)
     docs_dict.filter_extremes(no_below=5, no_above=0.2)
     docs_dict.compactify()
     return docs_dict
+
+# Preprocess docs
+def _preprocess(doc_list):
+    #Load spacy model
+    nlp  = spacy.load('en')
+    #lemmatise docs
+    docs = [_lemmatize_doc(nlp(doc)) for doc in doc_list] 
+    #Get docs dictionary
+    docs_dict = _get_docs_dict(docs)
+    return nlp, docs, docs_dict
 
 
 # Gensim can again be used to create a bag-of-words representation of each document,
@@ -42,19 +57,12 @@ def _document_vector(doc, docs_dict, nlp):
     return np.mean(doc_vector, axis=0)
 
 
-# Get a TF-IDF weighted Glove vector summary of each document
+# Get a TF-IDF weighted Glove vector summary for document list
 # Input: a list of documents, Output: Matrix of vector for all the documents
 def tfidf_weighted_wv(doc_list):
-    #####Initialize######
-    #Load spacy model
-    nlp  = spacy.load('en')
-
-    #lemmatise docs
-    docs = [_lemmatize_doc(nlp(doc)) for doc in doc_list]
+    #Initialize
+    nlp, docs, docs_dict = _preprocess(doc_list)
     
-    #Get docs dictionary
-    docs_dict = _get_docs_dict(docs)
-
     #tf-idf
     docs_vecs   = _get_tfidf(docs, docs_dict)
 
@@ -67,17 +75,18 @@ def tfidf_weighted_wv(doc_list):
     
     return docs_emb
 
-# Get TF-IDF vector for each document
-def get_tfidf(doc_list):
-    #####Initialize######
-    #Load spacy model
-    nlp  = spacy.load('en')
-
-    #lemmatise docs
-    docs = [_lemmatize_doc(nlp(doc)) for doc in doc_list]
+# Get average vector for document list
+def avg_wv(doc_list):
+    #Initialize
+    nlp, docs, docs_dict = _preprocess(doc_list)
     
-    #Get docs dictionary
-    docs_dict = _get_docs_dict(docs)
+    docs_vecs = np.vstack([_document_vector(doc, docs_dict, nlp) for doc in docs])
+    return docs_vecs
+
+# Get TF-IDF vector for document list
+def get_tfidf(doc_list):
+    #Initialize
+    nlp, docs, docs_dict = _preprocess(doc_list)
     
     docs_corpus = [docs_dict.doc2bow(doc) for doc in docs]
     model_tfidf = TfidfModel(docs_corpus, id2word=docs_dict)
@@ -86,18 +95,46 @@ def get_tfidf(doc_list):
     return docs_vecs
 
 
-
-# Get average vector for each document
-def avg_wv(doc_list):
-    #####Initialize######
-    #Load spacy model
-    nlp  = spacy.load('en')
-
-    #lemmatise docs
-    docs = [_lemmatize_doc(nlp(doc)) for doc in doc_list]
+# Get Latent Semantic Indexing(LSI) vector for document list
+def get_lsi(doc_list, num_topics=300):
+    #Initialize
+    nlp, docs, docs_dict = _preprocess(doc_list)
     
-    #Get docs dictionary
-    docs_dict = _get_docs_dict(docs)
+    docs_corpus = [docs_dict.doc2bow(doc) for doc in docs]
+    model_lsi = models.LsiModel(docs_corpus, num_topics, id2word=docs_dict)
+    docs_lsi  = model_lsi[docs_corpus]
+    docs_vecs   = np.vstack([sparse2full(c, len(docs_dict)) for c in docs_lsi])
+    return docs_vecs
+
+# Get Random Projections(RP) vector for document list
+def get_rp(doc_list):
+    #Initialize
+    nlp, docs, docs_dict = _preprocess(doc_list)
     
-    docs_vecs = np.vstack([_document_vector(doc, docs_dict, nlp) for doc in docs])
+    docs_corpus = [docs_dict.doc2bow(doc) for doc in docs]
+    model_rp = models.RpModel(docs_corpus, id2word=docs_dict)
+    docs_rp  = model_rp[docs_corpus]
+    docs_vecs   = np.vstack([sparse2full(c, len(docs_dict)) for c in docs_rp])
+    return docs_vecs
+
+# Get Latent Dirichlet Allocation(LDA) vector for document list
+def get_lda(doc_list, num_topics=100):
+    #Initialize
+    nlp, docs, docs_dict = _preprocess(doc_list)
+    
+    docs_corpus = [docs_dict.doc2bow(doc) for doc in docs]
+    model_lda = models.LdaModel(docs_corpus, num_topics, id2word=docs_dict)
+    docs_lda  = model_lda[docs_corpus]
+    docs_vecs   = np.vstack([sparse2full(c, len(docs_dict)) for c in docs_lda])
+    return docs_vecs
+
+# Get Hierarchical Dirichlet Process(HDP) vector for document list
+def get_hdp(doc_list):
+    #Initialize
+    nlp, docs, docs_dict = _preprocess(doc_list)
+    
+    docs_corpus = [docs_dict.doc2bow(doc) for doc in docs]
+    model_hdp = models.HdpModel(docs_corpus, id2word=docs_dict)
+    docs_hdp  = model_hdp[docs_corpus]
+    docs_vecs   = np.vstack([sparse2full(c, len(docs_dict)) for c in docs_hdp])
     return docs_vecs
